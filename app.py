777855c1738950dd97db39438ea8eda961523e66
luke <luke.nadackapadam@gmail.com>
2023-10-02 18:42:03 -0500

    Remove tkinter lib & edit to make code work with frontend

--- app.py
+++ app.py
@@ -1,108 +1,85 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import psycopg2
-from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from moviepy.editor import VideoFileClip
 import os
-from tkinter import Tk, filedialog
-
 
 app = Flask(__name__)
+CORS(app)
 
-# # Connect to the database
-# conn = psycopg2.connect(database="flask_db", user="postgres",
-# 						password="root", host="localhost", port="5432")
+# @app.route('/', methods=['GET'])
+# def index():
+# 	try:
+# 	# Connect to the database
+# 		conn = psycopg2.connect(database="video_clip_backend",
+# 								user="luken",
+# 								password="",
+# 								host="localhost", port="5432")
 
-# # create a cursor
-# cur = conn.cursor()
+# 	# create a cursor
+# 		cur = conn.cursor()	
 
-# # if you already have any table or not id doesnt matter this
-# # will create a products table for you.
-# cur.execute(
-# 	'''CREATE TABLE IF NOT EXISTS products (id serial \
-# 	PRIMARY KEY, name varchar(100), price float);''')
+# 	# Select all products from the table
+# 		cur.execute('''SELECT * FROM users''')
 
-# # Insert some data into the table
-# cur.execute(
-# 	'''INSERT INTO products (name, price) VALUES \
-# 	('Apple', 1.99), ('Orange', 0.99), ('Banana', 0.59);''')
+# 	# Fetch the data
+# 		data = cur.fetchall()
 
-# # commit the changes
-# conn.commit()
+# 	# close the cursor and connection
+# 		cur.close()
+# 		conn.close()
 
-# # close the cursor and connection
-# cur.close()
-# conn.close()
+# 		result = []
+# 		for row in data:
+# 				result.append({
+# 						'id': row[0],  # Assuming the first column is the ID
+# 						'email': row[1],  # Replace 'name' with your column names
+# 						'password': row[2],
+# 			})
 
+# 		return jsonify(result)
 
-@app.route('/', methods=['GET'])
-def index():
-	try:
-	# Connect to the database
-		conn = psycopg2.connect(database="video_clip_backend",
-								user="luken",
-								password="",
-								host="localhost", port="5432")
-
-	# create a cursor
-		cur = conn.cursor()	
-
-	# Select all products from the table
-		cur.execute('''SELECT * FROM users''')
-
-	# Fetch the data
-		data = cur.fetchall()
-
-	# close the cursor and connection
-		cur.close()
-		conn.close()
-
-		result = []
-		for row in data:
-				result.append({
-						'id': row[0],  # Assuming the first column is the ID
-						'email': row[1],  # Replace 'name' with your column names
-						'password': row[2],
-						# Add more fields as needed
-			})
-
-		return jsonify(result)
-
-	except Exception as e:
-		return jsonify({'error': str(e)})
+# 	except Exception as e:
+# 		return jsonify({'error': str(e)})
 
 
 @app.route('/subclip', methods=['POST'])
 def subclip():
-	# Create a Tkinter window to open the file dialog
-	root = Tk()
-	root.withdraw() # Hide the main window
+	input_video = request.files.get('video')
+	start_time = float(request.form.get('start_time', 0))
+	end_time = float(request.form.get('end_time', 0))
 
-# Ask the user to select the input video file
-input_video = filedialog.askopenfilename(initialdir=os.path.expanduser("~") + "/Downloads/")
+	if not input_video:
+		return jsonify({'error': 'No video file provided'}), 400
+	
+	try:
 
-if not input_video:
-    print("No video file selected. Exiting...")
-else:
-    # Set the start and end times for the subclip (in seconds)
-    start_time = float(input("Enter the start time (in seconds): "))  # Start time of the subclip
-    end_time = float(input("Enter the end time (in seconds): "))    # End time of the subclip
+			# Get the user's "Downloads" folder path
+		downloads_folder = os.path.expanduser("~") + "/Downloads/"
 
-    # Get the user's "Downloads" folder path
-    downloads_folder = os.path.expanduser("~") + "/Downloads/"
+			# Output subclip file name
+		output_subclip = downloads_folder + "output_subclip.mp4"
 
-    # Output subclip file name
-    output_subclip = downloads_folder + "output_subclip.mp4"
+			# Save the uploaded file to a temporary location
+		input_video.save('temp_video.mp4')
 
-    # Load the input video using VideoFileClip
-    video_clip = VideoFileClip(input_video)
+			# Load the input video using VideoFileClip
+		video_clip = VideoFileClip('temp_video.mp4')
 
-    # Create the subclip with both video and audio
-    subclip = video_clip.subclip(start_time, end_time)
+			# Create the subclip with both video and audio
+		subclip = video_clip.subclip(start_time, end_time)
 
-    # Write the subclip to a file with audio included
-    subclip.write_videofile(output_subclip, audio_codec='aac')
+			# Write the subclip to a file with audio included
+		subclip.write_videofile(output_subclip, audio_codec='aac')
 
-    print("Subclip with audio created successfully.")
+			# Clean up the temporary file
+		os.remove('temp_video.mp4')
+
+		return jsonify({"Message": "Subclip with audio created successfully."}), 200
+	
+	except Exception as e:
+		return jsonify({'error': str(e)}), 500
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
