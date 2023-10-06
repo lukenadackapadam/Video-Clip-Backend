c0ad930d6012e219609aaf6535d4447015e1f499
luke <luke.nadackapadam@gmail.com>
2023-10-06 12:38:15 -0500

    Add custom file_name + set default

--- app.py
+++ app.py
@@ -7,58 +7,32 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-# @app.route('/', methods=['GET'])
-# def index():
-# 	try:
-# 	# Connect to the database
-# 		conn = psycopg2.connect(database="video_clip_backend",
-# 								user="luken",
-# 								password="",
-# 								host="localhost", port="5432")
-
-# 	# create a cursor
-# 		cur = conn.cursor()	
-
-# 	# Select all products from the table
-# 		cur.execute('''SELECT * FROM users''')
-
-# 	# Fetch the data
-# 		data = cur.fetchall()
-
-# 	# close the cursor and connection
-# 		cur.close()
-# 		conn.close()
-
-# 		result = []
-# 		for row in data:
-# 				result.append({
-# 						'id': row[0],  # Assuming the first column is the ID
-# 						'email': row[1],  # Replace 'name' with your column names
-# 						'password': row[2],
-# 			})
-
-# 		return jsonify(result)
-
-# 	except Exception as e:
-# 		return jsonify({'error': str(e)})
-
-
 @app.route('/subclip', methods=['POST'])
 def subclip():
 	input_video = request.files.get('video')
 	start_time = float(request.form.get('start_time', 0))
 	end_time = float(request.form.get('end_time', 0))
+	file_name = (request.form.get('file_name'))
 
 	if not input_video:
 		return jsonify({'error': 'No video file provided'}), 400
 	
 	try:
 
+		# print(f"=====, {file_name}")
+		# file_name = 'abc'
+		# print(bool(file_name))
+		# file_name = ""
+		# print(bool(file_name))
+
 			# Get the user's "Downloads" folder path
 		downloads_folder = os.path.expanduser("~") + "/Downloads/"
 
 			# Output subclip file name
-		output_subclip = downloads_folder + "output_subclip.mp4"
+		if not file_name:
+			file_name = "outclip" 
+
+		output_subclip = downloads_folder + file_name + ".mp4"
 
 			# Save the uploaded file to a temporary location
 		input_video.save('temp_video.mp4')
